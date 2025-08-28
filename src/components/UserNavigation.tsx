af6835868ceb369c9f475effe88fe6e86efa7ff4
progress2002 <ezeamakachisom2002@gmail.com>
2025-08-28 10:12:26 +0100

    Conditionally render nav link

--- src/components/UserNavigation.tsx
+++ src/components/UserNavigation.tsx
@@ -3,29 +3,31 @@ import Logo from "./Logo";
 import { Button } from "./ui/button";
 import { HistoryIcon, LogOut } from "lucide-react";
 import { useLogout } from "@/hooks/useSignIn";
+import { UseAuth } from "@/contexts/AuthContext";
 
 export default function UserNavigation() {
   const { logOut, isLoggingOut } = useLogout();
+  const { currentUser } = UseAuth();
   return (
     <header className="bg-white fixed top-0 flex gap-3 justify-between px-5 md:px-12 w-full py-5 border-b border-b-border ">
       <Link to="/">
         <Logo />
       </Link>
       <nav className="flex gap-4">
-        <Link
-          to="/history"
-          className="font-base text-text-secondary hover:border-b border-primary-light delay-100 flex items-center gap-x-1 font-semibold text-sm"
-          aria-label="Report a bug"
-        >
-          <HistoryIcon />
-          <span className="hidden md:inline">Report</span> History
-        </Link>
+        {currentUser?.user_metadata.role === "student" && (
+          <Link
+            to="/history"
+            className="font-base text-text-secondary hover:border-b border-primary-light delay-100 flex items-center gap-x-1 font-semibold text-sm"
+            aria-label="Report a bug">
+            <HistoryIcon />
+            <span className="hidden md:inline">Report</span> History
+          </Link>
+        )}
         <Button
           variant="outline"
           className="cursor-pointer"
           disabled={isLoggingOut}
-          onClick={() => logOut()}
-        >
+          onClick={() => logOut()}>
           <LogOut color="red" />{" "}
           <span className="hidden md:inline">Log Out</span>
         </Button>
