d407377444cd98020d1419d08c9b3d4908dbd8bc
Adelopo Abdullah <68445551+ade-tech@users.noreply.github.com>
2025-08-28 12:05:39 +0100

    fixed the required form bug (#12)

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function History() {
   const { currentUser } = UseAuth();
-  const { data, isLoading } = useFetchReportsById(currentUser?.id!);
+  const { data, isLoading } = useFetchReportsById(currentUser?.id || "");
   const navigate = useNavigate();
   if (isLoading) {
     return (
@@ -25,10 +25,12 @@ export default function History() {
               <div className="ml-auto w-32 h-6 rounded-full bg-gray-100 animate-pulse"></div>
               <CardTitle
                 className="w-1/3 h-6 rounded-xl animate-pulse bg-gray-100
-                "></CardTitle>
+                "
+              ></CardTitle>
               <CardTitle
                 className="w-1/2 h-4 rounded-sm mt-1 mb-4 animate-pulse bg-gray-100
-                "></CardTitle>
+                "
+              ></CardTitle>
             </CardHeader>
           </Card>
         </div>
@@ -42,7 +44,8 @@ export default function History() {
         <Button
           className="my-5 cursor-pointer rounded-full border-primary-light bg-transparent py-1 px-3"
           variant="outline"
-          onClick={() => navigate("/")}>
+          onClick={() => navigate("/")}
+        >
           <ArrowLeft className="h-5 w-5 text-primary" />
         </Button>
         <div className="flex items-center gap-2 mb-6">
